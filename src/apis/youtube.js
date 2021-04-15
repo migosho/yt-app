f5c1b741741561b32f40b987b753d0b67eddd728
Migosho <migosho2015@gmail.com>
2021-04-15 14:45:02 -0700

    ready for deploy

---
+++ src/apis/youtube.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const KEY = 'AIzaSyCDaLJIfLQWkOQowuyy_a4ZrW0jGSdOnf4';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        type: 'video',
+        maxResults: 5,
+        key: KEY
+    }
+})
+
+
